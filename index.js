46fe0183ca0da75343edb5bc198dfeb9ca8d2c15
codkillah <tebedomeny@gmail.com>
2021-10-23 18:08:59 +0300

    finish_carusel

--- index.js
+++ index.js
@@ -1,70 +1,139 @@
 const root = document.getElementById('root');
-const mainSection = document.createElement('section');
-const instaLogo = document.createElement('img');
-const instaNameLogo = document.createElement('img');
 
-instaNameLogo.src =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png';
-instaLogo.src =
-  'https://www.instagram.com/static/images/homepage/home-phones.png/43cc71bb1b43.png';
+function createMainSection() {
+  const mainSection = document.createElement('section');
+  mainSection.classList.add('main_section');
 
-function createDivElement() {
-  let div = document.createElement('div');
-  return div;
-}
-const firstDivInWrapper = createDivElement();
-const secondDivInWrapper = createDivElement();
-const divInForm = createDivElement();
-const wrapper = createDivElement();
+  function createCaruselBlock() {
+    const div = document.createElement('div');
 
-function createInput() {
-  const input = document.createElement('input');
+    const imageSrc = [
+      'https://www.instagram.com/static/images/homepage/screenshot1.jpg/d6bf0c928b5a.jpg',
+      'https://www.instagram.com/static/images/homepage/screenshot2.jpg/6f03eb85463c.jpg',
+      'https://www.instagram.com/static/images/homepage/screenshot5.jpg/0a2d3016f375.jpg',
+      'https://www.instagram.com/static/images/homepage/screenshot3.jpg/f0c687aa6ec2.jpg',
+    ];
+    imageSrc.forEach((src) => {
+      const img = createImageBlock(src);
+      img.classList.add('carusel_block');
+      div.appendChild(img);
+    });
 
-  return input;
-}
+    return div;
+  }
 
-function createForm() {
-  const signInForm = document.createElement('form');
-  const inputEmail = createInput();
-  inputEmail.placeholder = 'Email-Name';
-  inputEmail.type = 'email';
-  inputEmail.name = 'email';
-  //
-  const inputPasword = createInput();
-  inputPasword.placeholder = 'Pasword';
-  inputPasword.type = 'password';
-  inputPasword.name = 'pasword';
-  //
-  const inputSubmit = createInput();
-  inputSubmit.type = 'submit';
-  inputSubmit.value = 'Login';
+  function createIPhoneBlock() {
+    const iPhoneBlock = document.createElement('div');
+    iPhoneBlock.appendChild(createCaruselBlock());
+    iPhoneBlock.classList.add('first_div');
 
-  //
-  signInForm.appendChild(instaNameLogo);
-  signInForm.appendChild(divInForm);
-  divInForm.appendChild(inputEmail);
-  divInForm.appendChild(inputPasword);
-  divInForm.appendChild(inputSubmit);
-  return signInForm;
-}
+    const div = document.createElement('div');
+    div.classList.add('main-carusel-block');
 
-function addCssClass() {
-  firstDivInWrapper.classList.add('first_div');
-  secondDivInWrapper.classList.add('second_div');
-  instaLogo.classList.add('img_style');
+    const iPhonePicture = createImageBlock(
+      'https://www.instagram.com/static/images/homepage/home-phones.png/43cc71bb1b43.png'
+    );
+    iPhonePicture.classList.add('img_style');
+    div.appendChild(iPhonePicture);
+    iPhoneBlock.appendChild(div);
+    return iPhoneBlock;
+  }
+
+  function createImageBlock(src) {
+    const img = document.createElement('img');
+    img.src = src;
+
+    return img;
+  }
+
+  const authorizationContainer = document.createElement('div');
+
+  const divInAuthorizationContainre = document.createElement('div');
+
+  const instaDivHeadingName = document.createElement('div');
+
+  const instaHeadingName = document.createElement('h1');
+
+  instaHeadingName.innerText = 'Instagram';
+
+  authorizationContainer.appendChild(divInAuthorizationContainre);
+
+  divInAuthorizationContainre.appendChild(instaDivHeadingName);
+
+  divInAuthorizationContainre.classList.add('authorization_form');
+
+  instaDivHeadingName.appendChild(instaHeadingName);
+
+  authorizationContainer.classList.add('second_div');
+
+  const wrapper = document.createElement('div');
+
   wrapper.classList.add('wrapper');
-  instaNameLogo.classList.add('insta_name_logo');
-  divInForm.classList.add('form_class');
-}
-addCssClass();
 
-function render() {
-  firstDivInWrapper.appendChild(instaLogo);
-  secondDivInWrapper.appendChild(createForm());
-  root.appendChild(mainSection);
+  //
+  function createSignInFormInput(inputObject) {
+    const input = document.createElement('input');
+    input.type = inputObject.type;
+
+    if (typeof inputObject.value !== 'undefined') {
+      input.value = inputObject.value;
+    }
+
+    if (typeof inputObject.placeholder !== 'undefined') {
+      input.placeholder = inputObject.placeholder;
+    }
+
+    if (typeof inputObject.name !== 'undefined') {
+      input.name = inputObject.name;
+    }
+
+    return input;
+  }
+
+  function createSignInForm() {
+    const signInForm = document.createElement('form');
+    signInForm.classList.add('form_class');
+    const emailInput = createSignInFormInput({
+      type: 'email',
+      placeholder: 'Email-Name',
+      name: 'email',
+    });
+    const passwordInput = createSignInFormInput({
+      type: 'password',
+      placeholder: 'Password',
+      name: 'password',
+    });
+    const submitButton = createSignInFormInput({
+      type: 'submit',
+      value: 'Login',
+    });
+
+    signInForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const fields = event.target.querySelectorAll(
+        'input[type=email], input[type=password]'
+      );
+      const values = {};
+
+      fields.forEach((field) => {
+        values[field.name] = field.value;
+      });
+      console.log(values);
+    });
+
+    signInForm.appendChild(emailInput);
+    signInForm.appendChild(passwordInput);
+    signInForm.appendChild(submitButton);
+
+    return signInForm;
+  }
   mainSection.appendChild(wrapper);
-  wrapper.appendChild(firstDivInWrapper);
-  wrapper.appendChild(secondDivInWrapper);
+  wrapper.appendChild(createIPhoneBlock());
+  wrapper.appendChild(authorizationContainer);
+  divInAuthorizationContainre.appendChild(createSignInForm());
+
+  return mainSection;
 }
 
-render();
+root.appendChild(createMainSection());
